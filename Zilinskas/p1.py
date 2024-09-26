8f03b7c357548cb01ab53dab37011ac46bcaae8b
Jayshil <jayshilpatel147@gmail.com>
2024-09-26 22:28:19 +0200

    Also storing substellar temperature

--- Zilinskas/p1.py
+++ Zilinskas/p1.py
@@ -8,7 +8,7 @@ import os
 tab = Table.read(os.getcwd() + '/Data/LavaPlanets/PlanetList/LavaPlanetsConfirmed1.csv', delimiter=',')
 tab_gmag = Table.read(os.getcwd() + '/Data/PSCompPars_2024.09.26_07.11.26.votable')
 
-stars = np.asarray(tab['st_name'])
+stars, subT = np.asarray(tab['st_name']), np.asarray(tab['pl_subT'])
 gmag = np.zeros(len(stars))
 
 tab_gmag_st, tab_gmag_gmag = np.asarray(tab_gmag['hostname']), np.asarray(tab_gmag['sy_gaiamag'])
@@ -26,5 +26,5 @@ for i in range(len(stars)):
 
 f1 = open(os.getcwd() + '/Zilinskas/Analysis/gmag.dat', 'w')
 for i in range(len(stars)):
-    f1.write(stars[i] + '\t\t' + str(gmag[i]) + '\n')
+    f1.write(stars[i] + '\t\t' + str(gmag[i]) + '\t\t' + str(subT[i]) + '\n')
 f1.close()
