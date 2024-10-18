d4e5ec4b12e4d118d1cb570c6b65d472dc4ab392
Jayshil <jayshilpatel147@gmail.com>
2024-10-18 15:01:28 +0200

    idealised case with only one lc

---
+++ Simulations/p1.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from exotoolbox.utils import tdur
+from exotoolbox import plots
+import matplotlib.gridspec as gd
+import juliet
+import utils
+import os
+
+# Simulations are for TOI-1807 b
+
+# Precision that we can get on 1 min data point is: 302.73 ppm (ETC). We will round it to 350 ppm (to account for gaps)
+# Or 68.23 (or, 70 ppm) in one transit duration.
+# Its expected depth is: 39.7 ppm -- so, to reach 3-sigma pecision we need to reach 13.23 ppm in 1 transit duration
+# For that we will need ~30 observations (because 70 / np.sqrt(30) = 12.78)
+# Idealised case: in this case, we will assume that we already observed ~30 observations and made an average of all of them
+# So, the 1 min precision in each light curve would be 350 / np.sqrt(30) = 63.9 ppm; 350 / np.sqrt(40) = 55.34
+# We will simulate 1 light curve with 1 min cadence and precision of 63.9 ppm; 55.34
+# Let's see how much we can extract
+pout = os.getcwd() + '/Simulations/Analysis/Idealised1807_30Obs'
+fpfs = 39.7*1e-6
+prec = 63.9*1e-6
+
+# Planetary parameters (From Nardiello et al. 2022)
+per, per_err =  0.549374, np.sqrt((0.000010**2) + (0.000013**2))
+tc, tc_err = 2458899.3449, np.sqrt((0.0008**2) + (0.0005**2))
+ar, ar_err = 3.8, 0.2
+bb, bb_err = 0.53, np.sqrt((0.09**2) + (0.11**2))
+rprs, rprs_err = 0.0182, 0.0006
+
+tsec, tsec_err = tc + (per/2), np.sqrt(tc_err**2 + (per_err/2)**2)
+
+t14 = tdur(per=per, ar=ar, rprs=rprs, bb=bb)
+
+# ------------------------------------------------------------------------------
+#
+#                              Simulation
+#
+# ------------------------------------------------------------------------------
+
+# Simulated times
+cadence = 1 / (60 * 24)
+times = np.arange(tsec-(1.5*t14), tsec+(1.5*t14), cadence)
+
+
+tim1, fl1, fle1 = utils.generate_occ_lc(tim=times, per=per, t0=tc, rprs=rprs, bb=bb, ar=ar, fp=fpfs, mflux=np.random.normal(0., 0.0001), prec=prec)
+
+
+# ------------------------------------------------------------------------------
+#
+#                              Retrieval
+#
+# ------------------------------------------------------------------------------
+
+instrument = 'TOI1807'
+tim, fl, fle = {}, {}, {}
+tim[instrument], fl[instrument], fle[instrument] = tim1, fl1, fle1
+
+# Priors
+## Planetary priors
+par_P = ['P_p1', 't0_p1', 'p_p1', 'b_p1', 'a_p1', 'ecc_p1', 'omega_p1', 't_secondary_p1', 'fp_p1']
+dist_P = ['fixed', 'fixed', 'fixed', 'fixed', 'fixed', 'fixed', 'fixed', 'normal', 'uniform']
+hyper_P = [per, tc, rprs, bb, ar, 0., 90., [tsec, tsec_err], [0.e-6, 100.e-6]]
+
+## Instrumental priors
+par_ins = ['mdilution_' + instrument, 'mflux_' + instrument, 'sigma_w_' + instrument]
+dist_ins = ['fixed', 'normal', 'loguniform']
+hyper_ins = [1.0, [0., 0.1], [0.1, 10000.]]
+
+priors = juliet.utils.generate_priors(par_P+par_ins, dist_P+dist_ins, hyper_P+hyper_ins)
+
+# And, fitting
+dataset = juliet.load(priors=priors, t_lc=tim, y_lc=fl, yerr_lc=fle, out_folder=pout)
+res = dataset.fit(sampler='dynesty')
+
+
+# And some plotting
+
+model = res.lc.evaluate(instrument)
+
+# Let's make sure that it works:
+fig = plt.figure(figsize=(16/1.5,9/1.5))
+gs = gd.GridSpec(2,1, height_ratios=[2,1])
+
+# Top panel
+ax1 = plt.subplot(gs[0])
+ax1.errorbar(tim[instrument], fl[instrument], yerr=fle[instrument], fmt='.', alpha=0.3)
+ax1.plot(tim[instrument], model, c='k', zorder=100)
+ax1.set_ylabel('Relative Flux')
+ax1.set_xlim(np.min(tim[instrument]), np.max(tim[instrument]))
+ax1.xaxis.set_major_formatter(plt.NullFormatter())
+
+# Bottom panel
+ax2 = plt.subplot(gs[1])
+ax2.errorbar(tim[instrument], (fl[instrument]-model)*1e6, yerr=fle[instrument]*1e6, fmt='.', alpha=0.3)
+ax2.axhline(y=0.0, c='black', ls='--', zorder=100)
+ax2.set_ylabel('Residuals (ppm)')
+ax2.set_xlabel('Time (BJD)')
+ax2.set_xlim(np.min(tim[instrument]), np.max(tim[instrument]))
+#plt.show()
+plt.savefig(pout + '/full_model_' + instrument + '.png')
+
+plots.corner_plot(pout)
---
+++ Simulations/utils.py
@@ -0,0 +1,94 @@
+import numpy as np
+import batman
+
+def generate_occ_lc(tim, per, t0, rprs, bb, ar, fp, mflux, prec):
+    # First generating a deterministic light curve
+    params = batman.TransitParams()
+    params.t0 = t0
+    params.per = per
+    params.rp = rprs
+    params.a = ar
+    params.inc = np.rad2deg(np.arccos(bb/ar))
+    params.ecc = 0.
+    params.w = 90.
+    params.u = [0.1, 0.3]
+    params.limb_dark = "quadratic"
+    params.fp = fp
+    params.t_secondary = t0 + (per/2)
+    m = batman.TransitModel(params, tim, transittype="secondary")
+    flux = m.light_curve(params)
+    deterministic = flux / (1 + mflux)
+
+    # And now simulated
+    fl, fle = np.zeros(len(tim)), np.zeros(len(tim))
+    for i in range(len(tim)):
+        fl[i] = np.random.normal(deterministic[i], prec)
+        fle[i] = np.random.normal(prec, 0.1*prec)
+    return tim, fl, fle
+
+def lcbin(time, flux, binwidth=0.06859, nmin=4, time0=None,
+        robust=False, tmid=False):
+    """
+    This code is taken from the code `pycheops`
+    Calculate average flux and error in time bins of equal width.
+    The default bin width is equivalent to one CHEOPS orbit in units of days.
+    To avoid binning data on either side of the gaps in the light curve due to
+    the CHEOPS orbit, the algorithm searches for the largest gap in the data
+    shorter than binwidth and places the bin edges so that they fall at the
+    centre of this gap. This behaviour can be avoided by setting a value for
+    the parameter time0.
+    The time values for the output bins can be either the average time value
+    of the input points or, if tmid is True, the centre of the time bin.
+    If robust is True, the output bin values are the median of the flux values
+    of the bin and the standard error is estimated from their mean absolute
+    deviation. Otherwise, the mean and standard deviation are used.
+    The output values are as follows.
+    * t_bin - average time of binned data points or centre of time bin.
+    * f_bin - mean or median of the input flux values.
+    * e_bin - standard error of flux points in the bin.
+    * n_bin - number of flux points in the bin.
+    :param time: time
+    :param flux: flux (or other quantity to be time-binned)
+    :param binwidth:  bin width in the same units as time
+    :param nmin: minimum number of points for output bins
+    :param time0: time value at the lower edge of one bin
+    :param robust: use median and robust estimate of standard deviation
+    :param tmid: return centre of time bins instead of mean time value
+    :returns: t_bin, f_bin, e_bin, n_bin
+    """
+    if time0 is None:
+        tgap = (time[1:]+time[:-1])/2
+        gap = time[1:]-time[:-1]
+        j = gap < binwidth
+        gap = gap[j]
+        tgap = tgap[j]
+        time0 = tgap[np.argmax(gap)]
+        time0 = time0 - binwidth*np.ceil((time0-min(time))/binwidth)
+
+    n = np.int(1+np.ceil(np.ptp(time)/binwidth))
+    r = (time0,time0+n*binwidth)
+    n_in_bin,bin_edges = np.histogram(time,bins=n,range=r)
+    bin_indices = np.digitize(time,bin_edges)
+
+    t_bin = np.zeros(n)
+    f_bin = np.zeros(n)
+    e_bin = np.zeros(n)
+    n_bin = np.zeros(n, dtype=np.int)
+
+    for i,n in enumerate(n_in_bin):
+        if n >= nmin:
+            j = bin_indices == i+1
+            n_bin[i] = n
+            if tmid:
+                t_bin[i] = (bin_edges[i]+bin_edges[i+1])/2
+            else:
+                t_bin[i] = np.nanmean(time[j])
+            if robust:
+                f_bin[i] = np.nanmedian(flux[j])
+                e_bin[i] = 1.25*np.nanmean(abs(flux[j] - f_bin[i]))/np.sqrt(n)
+            else:
+                f_bin[i] = np.nanmean(flux[j])
+                e_bin[i] = np.std(flux[j])/np.sqrt(n-1)
+
+    j = (n_bin >= nmin)
+    return t_bin[j], f_bin[j], e_bin[j], n_bin[j]
